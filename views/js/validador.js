482cbcf8821252afb9620385ae03c435f3b09f2e
karlaarenaata <202403307391@alunos.estacio.br>
2024-11-27 16:33:06 -0300

    alteraçoes

--- views/js/validador.js
+++ views/js/validador.js
@@ -16,21 +16,3 @@ function validarDadosPasseio() {
         return;
       }
 }
-
-function validarDadosCliente() {
-    const nome = document.getElementById("nome").value.trim();
-    const fone = document.getElementById("fone").value.trim();
-    const email = document.getElementById("email").value.trim();
-
-    if (!nome) {
-        alert("Por favor, preencha o campo Nome.");
-        return false;
-    }
-
-    if (!fone && !email) {
-        alert("Por favor, preencha pelo menos um dos campos: Telefone ou E-mail.");
-        return false;
-    }
-
-    return true;
-}
